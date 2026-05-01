e6ec27cb334f3b02bc33b23928b35e431d844630
fken57 <kenta20061017@gmail.com>
2026-05-01 19:08:31 +0900

    バックエンド作ったよ

---
+++ backend/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/jmoiron/sqlx"
+    _ "github.com/lib/pq"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.CORS())
+
+	e.GET("/hello", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World.\n")
+	})
+    
+	e.Logger.Fatal(e.Start(":8080")) 
+}
