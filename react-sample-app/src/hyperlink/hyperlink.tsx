155c2d52ff1786548dea1e50737c9943dab15771
fken57 <kenta20061017@gmail.com>
2026-05-01 19:35:35 +0900

    chore: cleanup repository and ignore node_modules

---
+++ react-sample-app/src/hyperlink/hyperlink.tsx
@@ -0,0 +1,12 @@
+
+
+
+function Hyperlink({ href, children }: { href: string; children: React.ReactNode }) {
+  return (
+    <a href={href} target="_blank" rel="noopener noreferrer">
+      {children}
+    </a>
+  );
+}
+
+export default Hyperlink;
